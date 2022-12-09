5ab7b5ef48604bf918c0570964ad42a85a78e15c
Ajit Kumar Yadav <ajitkumary768@gmail.com>
2022-12-09 09:48:56 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 23;
+        int mul = a*b;
+        System.out.println("The the product of two numbers : "+mul);
+    }
+    
+}
